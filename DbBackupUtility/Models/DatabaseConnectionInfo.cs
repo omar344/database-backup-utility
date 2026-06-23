8bf7cc4154fa2019d28736aecfeeb9d658a24331
omar344 <3marabadi@gmail.com>
2026-06-23 14:14:52 +0300

    fix: apply namespace to DatabaseConnectionInfo and cleanup

--- DbBackupUtility/Models/DatabaseConnectionInfo.cs
+++ DbBackupUtility/Models/DatabaseConnectionInfo.cs
@@ -1,17 +1,20 @@
-class DatabaseConnectionInfo
+namespace DbBackupUtility.Models
 {
-    public string Host { get; set; }
-    public int Port { get; set; }
-    public string DatabaseName { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
-
-    public DatabaseConnectionInfo(string host, int port, string databaseName, string username, string password)
+    public class DatabaseConnectionInfo
     {
-        Host = host;
-        Port = port;
-        DatabaseName = databaseName;
-        Username = username;
-        Password = password;
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string DatabaseName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public DatabaseConnectionInfo(string host, int port, string databaseName, string username, string password)
+        {
+            Host = host;
+            Port = port;
+            DatabaseName = databaseName;
+            Username = username;
+            Password = password;
+        }
     }
 }
